90f0a50443006ee0e054b209aadc9132716228c8
Guilherme <guil_stay@hotmail.com>
2018-08-27 23:28:14 -0300

    progres

--- Android/app/src/main/java/com/guilherme/usjtapp/MainActivity.java
+++ Android/app/src/main/java/com/guilherme/usjtapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.guilherme.usjtapp;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,7 +15,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("cicloDeVida", "MainActivity:onCreate");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("cicloDeVida", "MainActivity:onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("cicloDeVida", "MainActivity:onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("cicloDeVida", "MainActivity:onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("cicloDeVida", "MainActivity:onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("cicloDeVida", "MainActivity:onRestart");
     }
 
     public void sendMessage(View view){
